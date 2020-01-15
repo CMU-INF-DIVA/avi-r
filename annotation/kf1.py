1e4d636b2a62414693d71cf328fca135c8849b38
Lijun Yu <lijun@lj-y.com>
2020-01-15 10:22:06 -0500

    merge bbox format issue

--- annotation/kf1.py
+++ annotation/kf1.py
@@ -79,8 +79,9 @@ class KitwareAnnotation(object):
                     x1, y1, x2, y2 = geom['g0']
                     bbox_history[frame_id] = {
                         'presenceConf': 1,
-                        'boundingBox': {'x': x1, 'y': y1, 'w': x2 - x1,
-                                        'h': y2 - y1}}
+                        'boundingBox': {
+                            'x': min(x1, x2), 'y': min(y1, y2),
+                            'w': abs(x2 - x1), 'h': abs(y2 - y1)}}
                 for frame_id in range(start, end + 1):
                     if frame_id not in bbox_history:
                         bbox_history[frame_id] = {}
